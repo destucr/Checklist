90c66a681b08b6ec3d1be6c1f0a2068ae7f41852
Destu Cikal <destucr@gmail.com>
2026-01-30 23:38:30 +0700

    init

---
+++ Checklists/ChecklistViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Destu Cikal Ramdani on 1/28/26.
+//
+
+import UIKit
+import SwiftUI
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return 1000
+    }
+
+    override func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+
+        let label = cell.viewWithTag(1000) as! UILabel
+
+        if indexPath.row % 5 == 0 {
+            label.text = "walk the dog"
+        } else if indexPath.row % 5 == 1 {
+            label.text = "Brush my teeth"
+        } else if indexPath.row % 5 == 2 {
+            label.text = "Learn iOS development"
+        } else if indexPath.row % 5 == 3 {
+            label.text = "Soccer practice"
+        } else if indexPath.row % 5 == 4 {
+            label.text = "Eat ice cream"
+        }
+
+        return cell
+    }
+
+    override func tableView(
+        _ tableView: UITableView,
+        didSelectRowAt indexPath: IndexPath
+    ) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .none
+            {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
+struct UIViewControllerPreview<ViewController: UIViewController>: UIViewControllerRepresentable {
+
+    let builder: () -> ViewController
+
+    init(_ builder: @escaping () -> ViewController) {
+        self.builder = builder
+    }
+
+    func makeUIViewController(context: Context) -> ViewController {
+        builder()
+    }
+
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {}
+}
