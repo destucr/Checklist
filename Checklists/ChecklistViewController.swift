d12f1f54cb78410f6407b62683dbe58026bc6895
Destu Cikal <destucr@gmail.com>
2026-02-03 11:06:01 +0700

    feat add checkmark with toogle

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 import SwiftUI
 
 class ChecklistViewController: UITableViewController {
+    var row0checked = false
+    var row1checked = false
+    var row2checked = false
+    var row3checked = false
+    var row4checked = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,14 +55,29 @@ class ChecklistViewController: UITableViewController {
         didSelectRowAt indexPath: IndexPath
     ) {
         if let cell = tableView.cellForRow(at: indexPath) {
-            if cell.accessoryType == .none
-            {
+            var isChecked = false
+            if indexPath.row == 0 {
+                row0checked.toggle()
+                isChecked = row0checked
+            } else if indexPath.row == 1 {
+                row1checked.toggle()
+                isChecked = row1checked
+            } else if indexPath.row == 2 {
+                row2checked.toggle()
+                isChecked = row2checked
+            } else if indexPath.row == 3 {
+                row3checked.toggle()
+                isChecked = row3checked
+            } else if indexPath.row == 4 {
+                row4checked.toggle()
+                isChecked = row4checked
+            }
+
+            if isChecked {
                 cell.accessoryType = .checkmark
             } else {
                 cell.accessoryType = .none
             }
-        }
-
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
